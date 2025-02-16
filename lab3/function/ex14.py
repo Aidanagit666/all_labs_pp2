d42ae64065c787a2268c86afbfb7f25f5b867a45
Aidana <a_bazarbayeva@kbtu.kz>
2025-02-16 19:21:08 +0500

    update lab3

--- lab3/function/ex14.py
+++ lab3/function/ex14.py
@@ -1,18 +1,18 @@
-import 09, 10, 11, 13
+import ex09, ex10, ex11, ex13
 
 #Volume
 r = int(input("Radius of sphere - "))
-print(f'Volume of sphere = {09.v(r)}')
+print(f'Volume of sphere = {ex09.v(r)}')
 
 
 #Uniques
 l = list(map(int, input("write a list - ").split()))
-print(f'Uniques = {10.unique(l)}')
+print(f'Uniques = {ex10.unique(l)}')
 
 
 #Palindrom
 s = input("Write a sectence to check palindrom - ")
-if 11.palindrome(s):
+if ex11.palindrome(s):
     print("It's a palindrom")
 else:
     print("Nope, it isn't palindrom")
@@ -22,5 +22,5 @@ else:
 print("\nPlay a game, guess number 1:30")
 from random import randint
 mynum = randint(1, 30)
-att = 13.guess(mynum)
+att = ex13.guess(mynum)
 print(f"You're right, {att} - attemps needed")
